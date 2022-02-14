5eb885d0ce4ceed6b63c14fe3fee7e1df65cf12e
Dan Treacy <dan@blackswansolutions.net.au>
2022-02-14 17:30:31 +1100

    Update config

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,14 @@
 <?php
 //modify vars below
 // Database
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'dbuser');
-define('DB_PASSWORD', 'dbpass');
-define('DB_DATABASE', 'nflpickem');
-define('DB_PREFIX', 'nflp_');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'pickem2016');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
 
-define('SITE_URL', 'http://localhost/personal/applications/phppickem/');
+define('SITE_URL', 'http://pickem2016.test');
 define('ALLOW_SIGNUP', true);
 define('SHOW_SIGNUP_LINK', true);
 define('USER_NAMES_DISPLAY', 3); // 1 = real names, 2 = usernames, 3 = usernames w/ real names on hover
@@ -15,7 +16,7 @@ define('COMMENTS_SYSTEM', 'basic'); // basic, disqus, or disabled
 define('DISQUS_SHORTNAME', ''); // only needed if using Disqus for comments
 
 define('SEASON_YEAR', '2021');
-define('SERVER_TIMEZONE', 'America/Chicago'); // Your SERVER's timezone. NOTE: Game times will always be displayed in Eastern time, as they are on NFL.com. This setting makes sure cutoff times work properly.
+define('SERVER_TIMEZONE', 'UTC'); // Your SERVER's timezone. NOTE: Game times will always be displayed in Eastern time, as they are on NFL.com. This setting makes sure cutoff times work properly.
 
 // ***DO NOT EDIT ANYTHING BELOW THIS LINE***
 error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT ^ E_DEPRECATED);
